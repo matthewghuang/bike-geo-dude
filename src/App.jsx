86cec6955905d572b3fbdb671e81c18236d164f9
matthewghuang <matthewhhuang@gmail.com>
2021-06-19 11:37:20 -0700

    fix svg responsiveness, work on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,65 @@
 import React, { useState, useEffect, useLayoutEffect, useRef } from "react"
 import { Bike } from "./bike"
 import { BikeDisplay } from "./BikeDisplay"
-import { Grid } from "@material-ui/core"
+import {
+	Grid,
+	Typography,
+	Button,
+	Paper,
+	Box,
+	ThemeProvider,
+	CssBaseline,
+	responsiveFontSizes,
+	createMuiTheme
+} from "@material-ui/core"
+import { spacing } from "@material-ui/system"
+
+const theme = responsiveFontSizes(
+	createMuiTheme({
+		typography: {
+			fontFamily: "Roboto"
+		},
+		spacing: 8
+	})
+)
 
 export const App = () => {
 	const svg = useRef()
 
-	const [window_size, set_window_size] = useState({
-		width: window.innerWidth,
-		height: window.innerHeight
-	})
-
 	const [view_box, set_view_box] = useState("")
-
-	useLayoutEffect(() => {
-		const update_window_size = () =>
-			set_window_size({ width: window.innerWidth, height: window.innerHeight })
-
-		update_window_size()
-		window.addEventListener("resize", update_window_size)
-		return () => window.removeEventListener("resize", update_window_size)
-	}, [])
 
 	useLayoutEffect(() => {
 		const bbox = svg.current.getBBox()
 
 		set_view_box(
-			`${bbox.x - window_size.width / 2 + bbox.width / 2} ${
-				bbox.y - window_size.height / 2 + bbox.height / 2
-			} ${window_size.width} ${window_size.height}`
+			`${bbox.x - svg.current.clientWidth / 2 + bbox.width / 2} ${
+				bbox.y - svg.current.clientHeight / 2 + bbox.height / 2
+			} ${svg.current.clientWidth} ${svg.current.clientHeight}`
 		)
-	}, [svg.current])
-
-	const bike_one = new Bike({
-		wheel_diameter: 622,
-		chain_stay_length: 420,
-		bottom_bracket_drop: 70,
-		seat_tube_angle: 73.5,
-		seat_tube_length: 470,
-		wheel_base: 997,
-		head_tube_angle: 69,
-		head_tube_length: 90,
-		fork_length: 395,
-		fork_rake: 50,
-		seat_post_length: 660
-	})
-
-	const bike_two = new Bike({
-		wheel_diameter: 622,
-		chain_stay_length: 405,
-		bottom_bracket_drop: 70,
-		seat_tube_angle: 74.5,
-		seat_tube_length: 440,
-		effective_top_tube_length: 510,
-		head_tube_angle: 71,
-		head_tube_length: 135,
-		fork_length: 375,
-		fork_rake: 50,
-		seat_post_length: 660
-	})
+	}, [svg.current, window.innerWidth, window.innerHeight])
 
 	return (
-		<>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
 			<Grid container>
-				<Grid item xs={2}>
-					<h1>Bike Geo Dude</h1>
+				<Grid item xs={3}>
+					<Box p={1}>
+						<Typography variant="h6">Bike Geo Dude</Typography>
+						<Button variant="contained" color="primary">
+							Add a Bike
+						</Button>
+					</Box>
 				</Grid>
-				<Grid item xs={10}>
+				<Grid item xs={9}>
 					<svg
 						width="100%"
 						height="100%"
 						viewBox={view_box}
 						ref={svg}
 						className="col-span-10"
-					>
-						<BikeDisplay
-							bike={bike_one}
-							points={{ enabled: true, color: "purple" }}
-							tubes={{ enabled: true, color: "purple" }}
-						></BikeDisplay>
-						<BikeDisplay
-							bike={bike_two}
-							points={{ enabled: true, color: "green" }}
-							tubes={{ enabled: true, color: "green" }}
-						></BikeDisplay>
-					</svg>
+					></svg>
 				</Grid>
 			</Grid>
-		</>
+		</ThemeProvider>
 	)
 }
