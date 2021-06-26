7438d69c2dad17c209d0aad9a1a93fc25cc7ad29
matthewghuang <matthewhhuang@gmail.com>
2021-06-26 11:39:56 -0700

    ignore build in master

--- build/_snowpack/pkg/react-dom.js
+++
@@ -1,4 +0,0 @@
-import { r as reactDom } from './common/index-00d36f7e.js';
-export { r as default } from './common/index-00d36f7e.js';
-import './common/_commonjsHelpers-4f56b6ba.js';
-import './common/index-dfa77274.js';
